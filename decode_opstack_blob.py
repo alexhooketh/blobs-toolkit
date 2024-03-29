d30a21e5c15b5e44d20a1d54fae238596965e1b0
protolambda <proto@protolambda.com>
2024-03-29 14:55:45 -0600

    fixes

--- decode_opstack_blob.py
+++ decode_opstack_blob.py
@@ -9,38 +9,66 @@ def chunks(lst, n):
 # blobs from this tx: https://etherscan.io/tx/0x353c6f31903147f8d490c28e556caafd7a9fad8b3bc4fd210ae800ee24749adb
 blobs = open("opstack_blobs_19538908.bin", "rb").read()
 
-data = b""
+datas = []
 for blob in chunks(blobs, 131072):
     assert blob[1] == 0
     declared_length = int.from_bytes(blob[2:5])
     print("found version 0 blob, declared length:", declared_length)
     blob_data = b""
     for chunk in chunks(blob, 128): # split into chunks 4 field elements each
-        a = int.from_bytes(chunk[:32])
-        b = int.from_bytes(chunk[32:64])
-        c = int.from_bytes(chunk[64:96])
-        d = int.from_bytes(chunk[96:128])
-        assert (a | b | c | d) >> 254 == 0 # spec forces all elements to be max 254 bits
-        # helluva encoding (i won't comment this shit just copypaste it if you need it for your project)
-        a_dec = (((a & ((1 << 248) - 1)) << 8) | (a >> 248) | ((b >> 252) << 6)).to_bytes(32)
-        b_dec = (((b & ((1 << 248) - 1)) << 8) | ((b >> 248) & ((1 << 4) - 1)) | (((d >> 248) & ((1 << 4) - 1)) << 4)).to_bytes(32)
-        c_dec = (((c & ((1 << 248) - 1)) << 8) | (c >> 248) | ((d >> 252) << 6)).to_bytes(32)
-        d_dec = chunk[97:128]
-        blob_data += a_dec + b_dec + c_dec + d_dec
+
+        byteA = chunk[32*0]
+        byteB = chunk[32*1]
+        byteC = chunk[32*2]
+        byteD = chunk[32*3]
+
+        assert (byteA | byteB | byteC | byteD) & 0b1100_0000 == 0
+        tailA = chunk[32*0+1:32*1]
+        tailB = chunk[32*1+1:32*2]
+        tailC = chunk[32*2+1:32*3]
+        tailD = chunk[32*3+1:32*4]
+        
+        x = (byteA & 0b0011_1111) | ((byteB & 0b0011_0000) << 2)
+        y = (byteB & 0b0000_1111) | ((byteD & 0b0000_1111) << 4)
+        z = (byteC & 0b0011_1111) | ((byteD & 0b0011_0000) << 2)
+        
+        result = b""
+        result += tailA
+        result += x.to_bytes(1)
+        result += tailB
+        result += y.to_bytes(1)
+        result += tailC
+        result += z.to_bytes(1)
+        result += tailD
+        
+        assert len(result) == 4*31 + 3
+
+        blob_data += result
+
     
-    data += blob_data[4:declared_length+4]
+    datas.append(blob_data[4:declared_length+4])
 
-print("decoded blobs into %d bytes raw data" % len(data))
 channel = b""
-while data != b"":
-    assert data[0] == 0 # frame version
-    frame_length = int.from_bytes(data[19:23])
-    print("frame data length:", frame_length)
-    print("index", int.from_bytes(data[17:19]))
-    print("is last:", data[23+frame_length])
+for data in datas:
+    assert data[0] == 0 # derivation version
+    data = data[1:] # strip prefix byte
+    while data != b"":
+        print("remaining data bytes: %d" % len(data))
+        channel_id = data[0:16]
+        print("channel:", channel_id.hex())
+        frame_num = int.from_bytes(data[16:16+2])
+        print("frame num: %d" % frame_num)
+        frame_length = int.from_bytes(data[16+2:16+2+4])
+        print("frame data length:", frame_length)
+        end = 16+2+4+frame_length+1
+        print("is_last:", data[end-1:end])
+        frame_data = data[16+2+4:end-1]
+        channel += frame_data
+        data = data[end:]
 
-    channel += data[23:23+frame_length]
-    data = data[24+frame_length:]
+decomp = zlib.decompressobj()
+result = decomp.decompress(channel)
 
-print("channel data length:", len(channel))
-zlib.decompress(channel)
+print("result of %d bytes: %s..." % (len(result), result.hex()[:100]))
+
+
